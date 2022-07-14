6f022defceedfa0b24adb029afa95a5fac65d2bf
Rickyzie <a0935640996@gmail.com>
2022-07-14 23:36:30 +0800

    n

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import Header from './component/Header';
+import Item from './component/Item';
+function Home (){
+    return(
+        <div className='container'>
+            <Header />
+            <Item />
+        </div>
+    )
+};
+export default Home;
